f9de506354c29b57a5da878b0069aa0732210eff
Andrea <assilvi@libero.it>
2015-04-14 18:12:37 +0200

    library and todo

--- app/src/main/java/com/nightfall/navfriend/Main.java
+++ app/src/main/java/com/nightfall/navfriend/Main.java
@@ -4,14 +4,81 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.restlet.Restlet;
+import org.restlet.engine.connector.HttpClientHelper;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 
 public class Main extends ActionBarActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+
+
+
+
+        Button button = (Button) findViewById(R.id.submit);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText emailText = (EditText) v.findViewById(R.id.email) ;
+                EditText pwdText = (EditText) v.findViewById(R.id.pwd) ;
+
+                String email = emailText.getText().toString();
+                String pwd = pwdText.getText().toString();
+
+                /*
+                    TODO creare interfaccia della risorsa da chiedere
+                         http://restlet.com/technical-resources/restlet-framework/guide/2.2/introduction/first-steps/first-application
+                */
+
+
+            }
+        });
+
+/*
+        int TIMEOUT_MILLISEC = 10000;  // = 10 seconds
+        HttpParams httpParams = new BasicHttpParams();
+        HttpConnectionParams.setConnectionTimeout(httpParams, TIMEOUT_MILLISEC);
+        HttpConnectionParams.setSoTimeout(httpParams, TIMEOUT_MILLISEC);
+        HttpClient client = new DefaultHttpClient(httpParams);
+
+        HttpPost request = new HttpPost("http://192.168.201.114:8182");
+        try {
+            request.setEntity(new ByteArrayEntity("{}".toString().getBytes("UTF8")));
+            HttpResponse response = client.execute(request);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+*/
+
     }
 
 
