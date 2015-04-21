d2ce731a992df7ec6845bdd27791df50fa950fd3
Andrea Silvi <assilvi@libero.it>
2015-04-21 15:39:03 +0200

    aggiunta User

---
+++ app/src/main/java/com/nightfall/navfriend/data/User.java
@@ -0,0 +1,37 @@
+package com.nightfall.navfriend.data;
+
+public class User {
+
+    private String email;
+    private String pwd;
+
+    public User(){
+
+    }
+
+    public User(String email,String pwd){
+        this.email=email;
+        this.pwd=pwd;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString(){
+        return "email: "+email+", pwd: "+pwd;
+    }
+}
